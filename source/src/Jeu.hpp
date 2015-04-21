7135e0a438ed39c5e5463735f61f7dfeceb5f6c4
Cavaroc Clementine <ccavaroc@asi-testing>
2015-04-21 11:19:07 +0200

    doc jeu

--- source/src/Jeu.hpp
+++ source/src/Jeu.hpp
@@ -1,6 +1,10 @@
 #ifndef JEU_H
 #define JEU_H
 
+/// \file Jeu.hpp
+/// \brief fichier contenant la case Jeu
+/// \author
+
 #include "PersonnageNonJouable.hpp"
 #include "PersonnageJouable.hpp"
 #include "Monde.hpp"
@@ -9,16 +13,33 @@
 
 using namespace std;
 
+/// \class Jeu
+/// \brief déplacement sur la carte
+
 class Jeu{
-    public:
-        void initialiserJeu ();
-        bool partieFinie ();
-        void jouer(Coordonnees coordonnees);
+
     private:
-        Action actionEnCours;
-        vector<PersonnageNonJouable*> personnagesNonJouables;
-        PersonnageJouable* personnageJouable;
-        Monde monde;
-        Combat combat;
+        Action actionEnCours; /// action en cours
+        vector<PersonnageNonJouable*> personnagesNonJouables; ///liste de personnages non jouables
+        PersonnageJouable* personnageJouable; /// pointeur sur un personnage jouable
+        Monde monde; /// monde
+        Combat combat; /// combat
+
+    public:
+
+	/// \fn Jeu()
+	/// \brief Constructeur
+        Jeu();
+
+	/// \fn bool partieFinie()
+	/// \brief voir si partie finie
+	/// Renvoie vrai si le jeu est fini, sinon faux
+        bool partieFinie();
+
+	/// \fn void jouer(Coordonnees coordonnees)
+	/// \brief déplacement héros, lancement action
+	///  Déplace notre héros puis lance l'action
+        void jouer(Coordonnees coordonnees);
+
 };
 #endif
