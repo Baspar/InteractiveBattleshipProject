5afa1445b7ad5297678602d1512764c4b9e8331e
LePatron <qnguyen@insa-rouen.fr>
2015-05-15 17:12:07 +0200

    J'ai ecrit les DONE contents bande de connards ?

--- source/src/BatailleNavale.cpp
+++ source/src/BatailleNavale.cpp
@@ -8,21 +8,30 @@
 
 using namespace std;
 
-PersonnageBN* BatailleNavale::getPersonnage1(){
+PersonnageBN* BatailleNavale::getPersonnage1() const {//DONE
     return joueurs[0];
 }
 
-PersonnageBN* BatailleNavale::getPersonnage2(){
+PersonnageBN* BatailleNavale::getPersonnage2() const {//DONE
     return joueurs[1];
 }
 
-Grille BatailleNavale::getGrille1(){
+Grille BatailleNavale::getGrille1() const {//DONE
     return grilles[0];
 }
 
-Grille BatailleNavale::getGrille2(){
+Grille BatailleNavale::getGrille2() const {//DONE
     return grilles[1];
 }
+
+vector<Grille> BatailleNavale::getGrilles() const {//DONE
+    return grilles;
+}
+
+int BatailleNavale::getIndiceJoueurCourant() const {//DONE
+    return indiceJoueurCourant;
+}
+
 
 void BatailleNavale::jouer(Coordonnees coordonnees){//TODO
 }
