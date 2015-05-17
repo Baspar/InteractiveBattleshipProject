0d4d88039572368c94f515d71366cb5aa65a6e1b
damien <damien.pointin@insa-rouen.fr>
2015-05-17 17:37:45 +0100

    MainTestPersonnageBN

--- source/src/tests/MainTestPersonnageBN.cpp
+++ source/src/tests/MainTestPersonnageBN.cpp
@@ -8,5 +8,28 @@ int main(){
 	cout << "nom du joueur " << pbn.getNomBN() << endl;
 
 	cout << "Le PV initial du bateau 2 est " << pbn.getBateaux()[2]->getPV() << endl;
+	pbn.getBateaux()[0]->retirerPV();
+	pbn.getBateaux()[0]->retirerPV();
+
+	pbn.getBateaux()[1]->retirerPV();
+	pbn.getBateaux()[1]->retirerPV();
+	pbn.getBateaux()[1]->retirerPV();
+
+	pbn.getBateaux()[2]->retirerPV();
+	pbn.getBateaux()[2]->retirerPV();
+	pbn.getBateaux()[2]->retirerPV();
+
+	pbn.getBateaux()[3]->retirerPV();
+	pbn.getBateaux()[3]->retirerPV();
+	pbn.getBateaux()[3]->retirerPV();
+	pbn.getBateaux()[3]->retirerPV();
+
+	pbn.getBateaux()[4]->retirerPV();
+	pbn.getBateaux()[4]->retirerPV();
+	pbn.getBateaux()[4]->retirerPV();
+	pbn.getBateaux()[4]->retirerPV();
+	pbn.getBateaux()[4]->retirerPV();
+	cout << "Le PV initial du bateau 2 est " << pbn.getBateaux()[2]->getPV() << endl;
+	cout << "Coulee?? " << pbn.flotteCoulee() << endl;
 	
 }
