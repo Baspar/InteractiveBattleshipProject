b5f98060c5feb6ec24223be97da11174c1cb1346
Blaine <bastien.laine@insa-rouen.fr>
2015-05-17 17:58:36 +0200

    Modif Actionchangementcarte + Cellule + CelluleAccessible

--- source/src/ActionChangementCarte.cpp
+++ source/src/ActionChangementCarte.cpp
@@ -1,12 +1,22 @@
 #include "ActionChangementCarte.hpp"
 
 #include "Carte.hpp"
+#include "CelluleAccessible.hpp"
 #include "Coordonnees.hpp"
 #include "Action.hpp"
+#include "Personnage.hpp"
 
 using namespace std;
 
-void ActionChangementCarte::lancerAction(){//TODO
+void ActionChangementCarte::lancerAction(){//WIP
+    // Recuperation de la celluleInit et du perso
+    CelluleAccessible* cellInit = (CelluleAccessible*) carteInitiale->getCel(coordonneesInitiales);
+    Personnage* perso = celInit->getPersonnage();
+
+    // On retire le personnage
+    cellInit->setPersonnage(0);
 }
-ActionChangementCarte::ActionChangementCarte(){//TODO
+ActionChangementCarte::ActionChangementCarte(Carte* carteInit, Carte* carteDest, Coordonnees coordInit, Coordonnees coordDest) : coordonneesInitiales(coordInit),coordonneesDestination(coordDest) {//DONE
+    carteInitiale = carteInit;
+    carteDestination = carteDest;
 }
