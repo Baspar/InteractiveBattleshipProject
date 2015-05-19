50137ce490b64a3a47d4a4055c8dd0615fbd0bb8
ann <ann@besnard.com>
2015-05-19 12:45:16 +0200

    Main test cellule combat haha!!!

--- source/src/tests/MainTestCelluleCombat.cpp
+++ source/src/tests/MainTestCelluleCombat.cpp
@@ -2,10 +2,13 @@
 #include "CelluleCombat.hpp"
 #include "Personnage.hpp"
 #include "ActionCombat.hpp"
+#include "CelluleAccessible.hpp"
 
 int main(){
 	Personnage* advers = new Personnage("Ann");
-	//ActionCombat actCombat();
-	//CelluleCombat cell();
-	//CelluleCombat cellu(advers,&actCombat);
+	ActionCombat* actCombat = new ActionCombat(advers,"bla");
+	CelluleCombat cell();
+	CelluleCombat cellu(advers,actCombat);
+	cout << "cell est elle accesssible? " << cell.estAccessible() << endl;
+	cout << "cellu est elle accesssible? " << cellu.estAccessible() << endl;
 }
