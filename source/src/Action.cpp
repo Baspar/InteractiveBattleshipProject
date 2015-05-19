3c4ab9d9c18964a0b7cac2e0be19f3f80bb8172a
ann <ann@besnard.com>
2015-05-19 10:03:22 +0200

    Debug Carte

--- source/src/Action.cpp
+++ source/src/Action.cpp
@@ -3,16 +3,19 @@
 
 using namespace std;
 
+Action::Action(string txtInt){//DONE
+    active=true;
+    texteInteraction = txtInt;
+}
+
 void Action::toggleActive(){//DONE
     active=!active;
 }
+
 bool Action::isActive(){//DONE
     return active;
 }
-Action::Action(string txtInteraction){//DONE
-    active=true;
-    texteInteraction = txtInteraction;
-}
+
 string Action::getTexteInteraction(){//DONE
     return texteInteraction;
 }
