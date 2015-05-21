5f1f3f32ca11deb4d6767ad93ab8edc312096534
damien <damien.pointin@insa-rouen.fr>
2015-05-21 15:04:55 +0100

    Ajout Arme chercheuse dans liaison.uml + ajout //DONE

--- source/src/ArmeChercheuse.cpp
+++ source/src/ArmeChercheuse.cpp
@@ -10,7 +10,7 @@ ArmeChercheuse::ArmeChercheuse() : Arme("Arme Chercheuse"){//DONE
 
 
 
-void ArmeChercheuse::tirer(Coordonnees coordonnees, Grille* grilleAdverse){ //DONE
+void ArmeChercheuse::tirer(Coordonnees coordonnees, Grille* grilleAdverse){//DONE
   for(int i=0; i<grilleAdverse->getTailleGrille().getHauteur(); i++){
         for(int j=0; j<grilleAdverse->getTailleGrille().getLongueur(); j++){
             Case kase = grilleAdverse->getCaseElt(Coordonnees(j, i));
