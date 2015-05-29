490b7a6f3eb3dbd72f905ce897b38b6928006998
ann <ann@besnard.com>
2015-05-29 18:29:52 +0200

    MainTestActionCombat

--- source/src/tests/MainTestActionCombat.cpp
+++ source/src/tests/MainTestActionCombat.cpp
@@ -4,8 +4,10 @@
 
 using namespace std;
 
-int main{
+int main(){
 PersonnageNonJouable* adv = new PersonnageNonJouable("Martin");
 ActionCombat act(adv,"Ceci est le texte de l'Action Combat");
-
+cout << "L'adversaire est:" << adv << endl;
+cout << "L'adversaire est bien:" << act.getAdversaire() << endl;
+act.lancerAction();
 }
