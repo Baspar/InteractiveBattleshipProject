4ed1164f87818762a2a4fd699ce2a44f3af493d2
Blaine <bastien.laine@insa-rouen.fr>
2015-06-09 19:56:07 +0200

    Modif lecture perso

--- source/src/donnees/Transport.d
+++ source/src/donnees/Transport.d
@@ -1,6 +1,15 @@
-2
-0 6 10 1 4 1
-1 4 1 0 6 10
+10
+0 4 1 1 8 6
+1 1 7 4 1 4
+1 8 1 2 1 6
+1 8 3 4 1 4
+2 1 1 3 8 9
+2 9 1 1 8 6
+3 8 1 5 1 15
+4 3 1 1 8 6
+5 2 4 1 8 6
+5 9 2 6 1 6
+
 // nbTeleporteurs
 // idCarteDepart xCoordDepart yCoordDepart idCarteFin xCoordFin yCoordFin
 // ...
