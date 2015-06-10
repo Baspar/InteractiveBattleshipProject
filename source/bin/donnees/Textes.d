4bf312b621201c38c727430a2d37eb4ac767346b
Blaine <bastien.laine@insa-rouen.fr>
2015-06-10 17:35:28 +0200

    Ajout des textes + Tests OK

--- source/bin/donnees/Textes.d
+++ source/bin/donnees/Textes.d
@@ -1,3 +1,19 @@
-0
-    0 5 3 Tu te crois fort en Bataille Navale ?
-    0 5 4 On va voir รงa...
+18
+    1 2 3 De loin, je ressemble เ un huit, mais en fait je suis un humain.
+    1 6 3 L'homme a besoin de ce qu'il y a de pire en lui s'il veut parvenir a ce qu'il a de meilleur, disait Nietzsche
+	2 3 3 Il est tres agreable de se balader en maillot de bain et en tongs, surtout quand on a des examens.
+	2 5 3 J'aime manger
+	3 6 2 Je suis en Australie
+	3 2 2 Sais-tu combien font deux et deux ? Vingt-deux !
+	3 4 8 Je suis au Mexique
+	4 3 3 Je ne te cacherai pas que rester toute la journee dans une salle toute seule est un peu ennuyeux.
+	5 12 9 Salut
+	5 11 9 Je m'appelle Kevin
+	5 10 9 J'ai 21 ans
+	5 9 9 Je fais des maths
+	5 8 9 J'aime les kebabs
+	5 7 9 Je joue au tennis
+	5 6 9 Mon vrai prenom est Quoc-Duy
+	5 5 9 As-tu remarque que je suis bavard?
+	5 4 6 Il est possible que tu ne sois pas sur le bon chemin. Enfin, je dis ca, je dis rien.
+	6 5 2 Bonjour, je suis le BigBoss. Viens la si tu veux gagner !
