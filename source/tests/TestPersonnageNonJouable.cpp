e8fef1cd0ea35b68b2cb0617ef168d1b767c46c5
damien <damien.pointin@insa-rouen.fr>
2015-06-10 15:19:11 +0100

    personnageNonJOuable test ok

--- source/tests/TestPersonnageNonJouable.cpp
+++ source/tests/TestPersonnageNonJouable.cpp
@@ -7,10 +7,14 @@
 
 
 int main(){
-    PersonnageNonJouable jn("pôl");
+ 
 
-    //cout << "nom du joueur " << jn.getNomBN() << endl;
+cout << "Constructeur PersonnageNonJouable(string nomnv)" << endl;
+PersonnageNonJouable pj("pj");
+cout << "nom du joueur " << pj.getNom() << endl;
+cout << "Coordonnees de depart " << pj.getCoordonnees().getAbscisse() << "," << pj.getCoordonnees().getOrdonnee() << endl;
+cout << "Carte de depart " << pj.getCarte() << endl;
+cout << endl;
 
-
-    //cout << "Les PV initiaux du bateau 2: " << jn.getBateaux()[2]->getPV() << endl;
+   
 }
