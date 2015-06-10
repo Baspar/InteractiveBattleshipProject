e6857b6a75e329e069dce24938e423481c6e3dbf
vdesgrange <viviane.desgrange@insa-rouen.fr>
2015-06-10 20:21:04 +0200

    Ajout Test unit : Badge, BN, Bateau, Case

--- source/tests/TestCase.cpp
+++ source/tests/TestCase.cpp
@@ -8,6 +8,7 @@ Bateau bateau(5);
 Bateau* bat=&bateau;
 Case quase();
 Case kase(bat);
+cout << "le bateau a (PV) " << bat->getPV() << endl;
 kase.tirerDessus();
 cout << "le bateau a (PV) " << bat->getPV() << endl;
 kase.tirerDessus();
